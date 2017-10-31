07e0b744b6afe8d3027647e7a8edf30280b754fb
Hanna Tasya <hannaatasya@gmail.com>
2017-10-31 22:36:21 +0700

    hari pertama

---
+++ main.py
@@ -0,0 +1,13 @@
+map_1 = folium.Map(location=[-6.1765123,106.5799927], zoom_start=12,
+                   tiles='stamen ')
+folium.Marker([-6.220371, 106.662390], popup='IKEA Alam Sutera').add_to(map_1)
+folium.Marker([-6.241532, 106.628401], popup='Summarecon Mal').add_to(map_1)
+folium.Marker([-6.127871, 106.652777], popup='Bandar Udara Internasional Soekarno-Hatta').add_to(map_1)
+folium.Marker([-6.193067, 106.680930], popup='Danau Cipondoh').add_to(map_1)
+folium.Marker([-6.158891, 106.583855], popup='Amsterdam Water Park').add_to(map_1)
+folium.Marker([-6.158891, 106.583855], popup='Sekolah Tarsisius Vireta').add_to(map_1)
+folium.Marker([-6.158891, 106.583855], popup='Global Mansion').add_to(map_1)
+folium.Marker([-6.169963, 106.595013], popup='SMA 15 Tangerang').add_to(map_1)
+folium.Marker([-6.187072, 106.603596], popup='Giant Ekstra Gatot Subroto').add_to(map_1)
+folium.Marker([-6.187072, 106.603596], popup='Taman Cibodas Raya').add_to(map_1)
+map_1

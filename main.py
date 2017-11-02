9e77bc0b63d540f59c6513a2d88f75aa9c94e629
tiasmaulana <tias.maulanaa@gmail.com>
2017-11-02 23:18:14 +0700

    Tias Maulana

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import folium
-map_1 = folium.Map(location=[-6.1765123,106.5799927], zoom_start=12,
-                   tiles='Tangerang ')
-folium.Marker([-6.220371, 106.662390], popup='IKEA Alam Sutera').add_to(map_1)
-folium.Marker([-6.241532, 106.628401], popup='Summarecon Mal').add_to(map_1)
-folium.Marker([-6.127871, 106.652777], popup='Bandar Udara Internasional Soekarno-Hatta').add_to(map_1)
-folium.Marker([-6.193067, 106.680930], popup='Danau Cipondoh').add_to(map_1)
-folium.Marker([-6.158891, 106.583855], popup='Amsterdam Water Park').add_to(map_1)
-folium.Marker([-6.158891, 106.583855], popup='Sekolah Tarsisius Vireta').add_to(map_1)
-folium.Marker([-6.158891, 106.583855], popup='Global Mansion').add_to(map_1)
-folium.Marker([-6.169963, 106.595013], popup='SMA 15 Tangerang').add_to(map_1)
-folium.Marker([-6.187072, 106.603596], popup='Giant Ekstra Gatot Subroto').add_to(map_1)
-folium.Marker([-6.187072, 106.603596], popup='Taman Cibodas Raya').add_to(map_1)
-folium.Marker([-6.177019, 106.650449], popup='Warung Makan Mama Laras').add_to(map_1)
-folium.Marker([-6.177606, 106.646157], popup='Butik Pelangi').add_to(map_1)
-folium.Marker([-6.176699, 106.653163], popup='Masjid Jami Al-Hikmah Tangerang').add_to(map_1)
-folium.Marker([-6.175238, 106.646297], popup='Stasiun Tanah Tinggi').add_to(map_1)
-folium.Marker([-6.174875, 106.652831], popup='Pertamina Spbu 34-15129').add_to(map_1)
-folium.Marker([-6.185595, 106.643679], popup='Apotek Berkat').add_to(map_1)
-folium.Marker([-6.188422, 106.642231], popup='Rumah Makan Cak Mun').add_to(map_1)
-folium.Marker([-6.186651, 106.637757], popup='SMK Negeri 4 Tangerang').add_to(map_1)
-folium.Marker([-6.191152, 106.640289], popup='BRI Tbk.PT').add_to(map_1)
-folium.Marker([-6.194096, 106.641823], popup='Perumahan Cluster Pasadena').add_to(map_1)
-map_1
+
+m = folium.Map(
+    location=[-6.202367, 106.655953],
+    zoom_start=12,
+    tiles='Stamen Terrain'
+
+)
+
+tooltip = 'Click me!'
+
+folium.Marker([-6.220371, 106.662390], popup='IKEA Alam Sutera').add_to(m)
+folium.Marker([-6.241532, 106.628401], popup='Summarecon Mal').add_to(m)
+folium.Marker([-6.127871, 106.652777], popup='Bandar Udara Internasional Soekarno-Hatta').add_to(m)
+folium.Marker([-6.193067, 106.680930], popup='Danau Cipondoh').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Amsterdam Water Park').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Sekolah Tarsisius Vireta').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Global Mansion').add_to(m)
+folium.Marker([-6.169963, 106.595013], popup='SMA 15 Tangerang').add_to(m)
+folium.Marker([-6.187072, 106.603596], popup='Giant Ekstra Gatot Subroto').add_to(m)
+folium.Marker([-6.187072, 106.603596], popup='Taman Cibodas Raya').add_to(m)
+folium.Marker([-6.177019, 106.650449], popup='Warung Makan Mama Laras').add_to(m)
+folium.Marker([-6.177606, 106.646157], popup='Butik Pelangi').add_to(m)
+folium.Marker([-6.176699, 106.653163], popup='Masjid Jami Al-Hikmah Tangerang').add_to(m)
+folium.Marker([-6.175238, 106.646297], popup='Stasiun Tanah Tinggi').add_to(m)
+folium.Marker([-6.174875, 106.652831], popup='Pertamina Spbu 34-15129').add_to(m)
+folium.Marker([-6.185595, 106.643679], popup='Apotek Berkat').add_to(m)
+folium.Marker([-6.188422, 106.642231], popup='Rumah Makan Cak Mun').add_to(m)
+folium.Marker([-6.186651, 106.637757], popup='SMK Negeri 4 Tangerang').add_to(m)
+folium.Marker([-6.191152, 106.640289], popup='BRI Tbk.PT').add_to(m)
+folium.Marker([-6.194096, 106.641823], popup='Perumahan Cluster Pasadena').add_to(m)
+folium.Marker([-6.200458, 106.660009], popup='<i>Ternak Lele Pak Napis</i>').add_to(m)
+folium.Marker([-6.193333, 106.673537], popup='<i>KFC Cipondoh</i>').add_to(m)
+folium.Marker([-6.188544, 106.664965], popup='<i>McDonald’s Cipondoh</i>').add_to(m)
+folium.Marker([-6.181113, 106.661245], popup='<i>Kings Furniture Tangerang</i>').add_to(m)
+folium.Marker([-6.187876, 106.678325], popup='<i>Kongkow Village Cafe</i>').add_to(m)
+folium.Marker([-6.188004, 106.658262], popup='<i>Kolam Renang Banjar Wijaya</i>').add_to(m)
+folium.Marker([-6.175353, 106.663755], popup='<i>Pasar Royal</i>').add_to(m)
+folium.Marker([-6.171430, 106.646498], popup='<i>Stasiun Geofisika Tangerang</i>').add_to(m)
+folium.Marker([-6.171526, 106.649170], popup='<i>Nasi Goreng Mas Jangkung</i>').add_to(m)
+folium.Marker([-6.170075, 106.646874], popup='<i>Kantor Kelurahan Tanah Tinggi</i>').add_to(m)
+folium.Marker([-6.169307, 106.644975], popup='<i> Masjid Al Hakim</i>').add_to(m)
+folium.Marker([-6.167344, 106.646842], popup='<i>Komp.LP Wanita tanah tinggi</i>').add_to(m)
+folium.Marker([-6.167984, 106.648193], popup='<i>Masjid AL Islah</i>').add_to(m)
+folium.Marker([-6.169040, 106.649331], popup='<i>Masjid AL Hikmah</i>').add_to(m)
+folium.Marker([-6.168779, 106.647544], popup='<i>DeZie Cafe</i>').add_to(m)
+folium.Marker([-6.170065, 106.649191], popup='<i>Warung Sate Solo</i>').add_to(m)
+folium.Marker([-6.170342, 106.647501], popup='<i>Lembaga Kebijakan Publik</i>').add_to(m)
+folium.Marker([-6.170987, 106.643269], popup='<i>Komplek Imigrasi Tangerang</i>').add_to(m)
+folium.Marker([-6.172689, 106.640130], popup='<i>Lapangan Sepak Bola Sukun</i>').add_to(m)
+folium.Marker([-6.172747, 106.640796], popup='<i>Lapangan Tembak PERBAKIN Tangerang</i>').add_to(m)
+
+m

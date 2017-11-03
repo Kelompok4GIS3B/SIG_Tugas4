4cb2ce9faa3d9d82ffc7e4697c6d7c8faa2ffe06
Hanna Tasya <hannaatasya@gmail.com>
2017-11-03 14:43:37 +0700

    harikeempat

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 ﻿import folium
 
-m = folium.Map(
-    location=[-6.202367, 106.655953],
+def Marker(long,lat):
+	m = folium.Map(
+    location=[long,lat],
     zoom_start=12,
-    tiles='Stamen Terrain'
+    tiles='Stamen Terrain')
+	return m
 
-)
-
+def RegularPolygon(long,lat):
+	a = folium.Map(
+    location=[long,lat],
+    zoom_start=12,
+    tiles='Stamen Toner')	
+	return a
+	
+def Circle(long,lat):
+	n = folium.Map(
+    location=[long,lat],
+    zoom_start=12,
+    tiles='Stamen Toner')	
+	return n
+	
+def disimpan(empat,lima):
+	empat.save(lima)
+	
+n = Circle(-6.202367, 106.655953)
+a = RegularPolygon(-6.202367, 106.655953)
+m = Marker(-6.202367, 106.655953)
 tooltip = 'Click me!'
 
-folium.Marker([-6.220371, 106.662390], popup='IKEA Alam Sutera').add_to(m)
-folium.Marker([-6.241532, 106.628401], popup='Summarecon Mal').add_to(m)
-folium.Marker([-6.127871, 106.652777], popup='Bandar Udara Internasional Soekarno-Hatta').add_to(m)
-folium.Marker([-6.193067, 106.680930], popup='Danau Cipondoh').add_to(m)
-folium.Marker([-6.158891, 106.583855], popup='Amsterdam Water Park').add_to(m)
-folium.Marker([-6.158891, 106.583855], popup='Sekolah Tarsisius Vireta').add_to(m)
-folium.Marker([-6.158891, 106.583855], popup='Global Mansion').add_to(m)
-folium.Marker([-6.169963, 106.595013], popup='SMA 15 Tangerang').add_to(m)
-folium.Marker([-6.187072, 106.603596], popup='Giant Ekstra Gatot Subroto').add_to(m)
-folium.Marker([-6.187072, 106.603596], popup='Taman Cibodas Raya').add_to(m)
-folium.Marker([-6.177019, 106.650449], popup='Warung Makan Mama Laras').add_to(m)
-folium.Marker([-6.177606, 106.646157], popup='Butik Pelangi').add_to(m)
-folium.Marker([-6.176699, 106.653163], popup='Masjid Jami Al-Hikmah Tangerang').add_to(m)
-folium.Marker([-6.175238, 106.646297], popup='Stasiun Tanah Tinggi').add_to(m)
-folium.Marker([-6.174875, 106.652831], popup='Pertamina Spbu 34-15129').add_to(m)
-folium.Marker([-6.185595, 106.643679], popup='Apotek Berkat').add_to(m)
-folium.Marker([-6.188422, 106.642231], popup='Rumah Makan Cak Mun').add_to(m)
-folium.Marker([-6.186651, 106.637757], popup='SMK Negeri 4 Tangerang').add_to(m)
-folium.Marker([-6.191152, 106.640289], popup='BRI Tbk.PT').add_to(m)
-folium.Marker([-6.194096, 106.641823], popup='Perumahan Cluster Pasadena').add_to(m)
 folium.Marker([-6.200458, 106.660009], popup='<i>Ternak Lele Pak Napis</i>').add_to(m)
 folium.Marker([-6.193333, 106.673537], popup='<i>KFC Cipondoh</i>').add_to(m)
 folium.Marker([-6.188544, 106.664965], popup='<i>McDonald’s Cipondoh</i>').add_to(m)
@@ -61,17 +61,8 @@ folium.Marker([-6.202922, 106.688569], popup='<i>GREEN VILLAGE</i>').add_to(m)
 folium.Marker([-6.206527, 106.687163], popup='<i>Dojo Cipondoh</i>').add_to(m)
 folium.Marker([-6.203828, 106.687904], popup='<i>Global Cell</i>').add_to(m)
 folium.Marker([-6.202527, 106.688333], popup='<i>Cipondoh Jaya Motor</i>').add_to(m)
-folium.Marker([-6.190755, 106.638422], popup='Akar Sabu').add_to(m)
-folium.Marker([-6.189870, 106.634554], popup='Graha Malika').add_to(m)
-folium.Marker([-6.189704, 106.636271], popup='UNIS Tangerang').add_to(m)
-folium.Marker([-6.196628, 106.639260], popup='Warung Kopi Bang Ade').add_to(m)
-folium.Marker([-6.196554, 106.641432], popup='Ayam Tulang Lunak Bu Iin').add_to(m)
-folium.Marker([-6.204857, 106.638750], popup='Perumahan Cluster Pulau Dewa Modernland').add_to(m)
-folium.Marker([-6.204553, 106.641304], popup='Bakmi Ayam Ijo Mayapada').add_to(m)
-folium.Marker([-6.177683, 106.625165], popup='RS Waras').add_to(m)
-folium.Marker([-6.179688, 106.621367], popup='Hotel Merdeka').add_to(m)
-folium.Marker([-6.185360, 106.620166], popup='Pakis Mas Resindence').add_to(m)
-<<<<<<< HEAD:main.py.py
+
+
 folium.Marker([-6.172928, 106.664536], popup='Poris Plawad').add_to(m)
 folium.Marker([-6.162902, 106.667518], popup='BPJS Kesehatan Batu Ceper').add_to(m)
 folium.Marker([-6.163083, 106.671510], popup='Jl. Daan Mogot No.289').add_to(m)
@@ -95,6 +86,83 @@ folium.Marker([-6.195503, 106.653694], popup='Cluster Nirwana Kota Modern').add_
 folium.Marker([-6.200795, 106.655701], popup='Dufia isi Ulang').add_to(m)
 folium.Marker([-6.199384, 106.655056], popup='Water Park Taman Konblock').add_to(m)
 folium.Marker([-6.200544, 106.658478], popup='Kantor Kelurahan Cipete').add_to(m)
->>>>>>> 6cbc200e4419c7a5b0e5432ea82ad2d7475685a8:main.py
 
+
+%punya hanna
+
+folium.Marker([-6.220371, 106.662390], popup='IKEA Alam Sutera').add_to(m)
+folium.Marker([-6.241532, 106.628401], popup='Summarecon Mal').add_to(m)
+folium.Marker([-6.127871, 106.652777], popup='Bandar Udara Internasional Soekarno-Hatta').add_to(m)
+folium.Marker([-6.193067, 106.680930], popup='Danau Cipondoh').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Amsterdam Water Park').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Sekolah Tarsisius Vireta').add_to(m)
+folium.Marker([-6.158891, 106.583855], popup='Global Mansion').add_to(m)
+folium.Marker([-6.169963, 106.595013], popup='SMA 15 Tangerang').add_to(m)
+folium.Marker([-6.187072, 106.603596], popup='Giant Ekstra Gatot Subroto').add_to(m)
+folium.Marker([-6.187072, 106.603596], popup='Taman Cibodas Raya').add_to(m)
+folium.Marker([-6.177019, 106.650449], popup='Warung Makan Mama Laras').add_to(m)
+folium.Marker([-6.177606, 106.646157], popup='Butik Pelangi').add_to(m)
+folium.Marker([-6.176699, 106.653163], popup='Masjid Jami Al-Hikmah Tangerang').add_to(m)
+folium.Marker([-6.175238, 106.646297], popup='Stasiun Tanah Tinggi').add_to(m)
+folium.Marker([-6.174875, 106.652831], popup='Pertamina Spbu 34-15129').add_to(m)
+folium.Marker([-6.185595, 106.643679], popup='Apotek Berkat').add_to(m)
+folium.Marker([-6.188422, 106.642231], popup='Rumah Makan Cak Mun').add_to(m)
+folium.Marker([-6.186651, 106.637757], popup='SMK Negeri 4 Tangerang').add_to(m)
+folium.Marker([-6.191152, 106.640289], popup='BRI Tbk.PT').add_to(m)
+folium.Marker([-6.194096, 106.641823], popup='Perumahan Cluster Pasadena').add_to(m)
+folium.Marker([-6.190755, 106.638422], popup='Akar Sabu').add_to(m)
+folium.Marker([-6.189870, 106.634554], popup='Graha Malika').add_to(m)
+folium.Marker([-6.189704, 106.636271], popup='UNIS Tangerang').add_to(m)
+folium.Marker([-6.196628, 106.639260], popup='Warung Kopi Bang Ade').add_to(m)
+folium.Marker([-6.196554, 106.641432], popup='Ayam Tulang Lunak Bu Iin').add_to(m)
+folium.Marker([-6.204857, 106.638750], popup='Perumahan Cluster Pulau Dewa Modernland').add_to(m)
+folium.Marker([-6.204553, 106.641304], popup='Bakmi Ayam Ijo Mayapada').add_to(m)
+folium.Marker([-6.177683, 106.625165], popup='RS Waras').add_to(m)
+folium.Marker([-6.179688, 106.621367], popup='Hotel Merdeka').add_to(m)
+folium.Marker([-6.185360, 106.620166], popup='Pakis Mas Resindence').add_to(m)
+folium.Marker([-6.214656, 106.676742], popup='Starbuck Rest AREA KM 14').add_to(m)
+folium.Marker([-6.213085, 106.679135], popup='Pecel Lele Depot Cantik Asih').add_to(m)
+folium.Marker([-6.221378, 106.679543], popup='Sekolah Dasar Islam Baiturrachman').add_to(m)
+folium.Marker([-6.220125, 106.677419], popup='My Shin Cafe').add_to(m)
+folium.Marker([-6.220125, 106.677419], popup='Kantor Kelurahan Cipete').add_to(m)
+folium.Marker([-6.202619, 106.660007], popup='Gumay Foundation').add_to(m)
+folium.Marker([-6.196998, 106.665049], popup='Pandan Aquarium Toko').add_to(m)
+folium.Marker([-6.194565, 106.671339], popup='LPK HIKARI GAKKAI').add_to(m)
+folium.Marker([-6.200840, 106.650914], popup='SOYES HOME Soya Milk').add_to(m)
+folium.Marker([-6.192851, 106.662379], popup='Masjid Quba Taman Royal 2').add_to(m)
+folium.RegularPolygonMarker(
+    [-6.191979, 106.591591],
+    popup='RS Dinda',
+    fill_color='#132b5e',
+    number_of_sides=3,
+    radius=10
+    ).add_to(a)
+folium.RegularPolygonMarker(
+    [-6.195179, 106.585679],
+    popup='BPJS Liasion Office Jatake',
+    fill_color='#132b5e',
+    number_of_sides=3,
+    radius=10
+    ).add_to(a)	
+a
+
+folium.Circle(
+    radius=100,
+    location=[-6.189003, 106.579832],
+    popup='PT. Pancaprima Ekabrother',
+    color='crimson',
+    fill=False,
+).add_to(n)
+n
+
+
+folium.Marker(
+    location=[-6.182490, 106.584710],
+    popup='Distibutor NASA N-430321',
+    icon=folium.Icon(icon='cloud')
+).add_to(m)
 m
+
+disimpan(a,'4.html')
+disimpan(n,'5.html')
+disimpan(m,'6.html')
